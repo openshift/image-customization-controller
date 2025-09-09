openshift/image-customization-controller

commit 583e13f963e7b47fdf8b92c908e65142d14a493b

Add support for multi arch image files
When we start, we read the files in the shared directory and load them
into a map based on architecture.  Then, when we serve an image, we look
at the architecture field in the PreprovisioningImage and look up the
respective files.

pkg/env/env.go
@@ -10,6 +10,7 @@ import (
 type EnvInputs struct {
 	DeployISO                 string `envconfig:"DEPLOY_ISO" required:"true"`
 	DeployInitrd              string `envconfig:"DEPLOY_INITRD" required:"true"`
+	ImageSharedDir            string `envconfig:"IMAGE_SHARED_DIR" default:"/shared/html/images"`
 	IronicBaseURL             string `envconfig:"IRONIC_BASE_URL"`
 	IronicInspectorBaseURL    string `envconfig:"IRONIC_INSPECTOR_BASE_URL"`
 	IronicAgentImage          string `envconfig:"IRONIC_AGENT_IMAGE" required:"true"`

pkg/imagehandler/imagehandler.go
@@ -17,11 +17,58 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"os"
+	"path"
+	"regexp"
 	"sync"
 
 	"github.com/go-logr/logr"
 	"github.com/google/uuid"
+
+	"github.com/openshift/image-customization-controller/pkg/env"
 )
+
+const (
+	hostArchitectureKey = "host"
+)
+
+var deployImagePattern = regexp.MustCompile(`ironic-python-agent_(\w+)\.(iso|initramfs)`)
+
+type ironicImage struct {
+	filename string
+	arch     string
+	iso      bool
+}
+
+func parseDeployImage(envInputs *env.EnvInputs, filename string) (ironicImage, error) {
+	if filename == envInputs.DeployISO {
+		return ironicImage{
+			filename: filename,
+			arch:     hostArchitectureKey,
+			iso:      true,
+		}, nil
+	}
+
+	if filename == envInputs.DeployInitrd {
+		return ironicImage{
+			filename: filename,
+			arch:     hostArchitectureKey,
+			iso:      false,
+		}, nil
+	}
+
+	matches := deployImagePattern.FindStringSubmatch(filename)
+
+	if len(matches) != 3 {
+		return ironicImage{}, fmt.Errorf("failed to parse ironic image name: %s", filename)
+	}
+
+	return ironicImage{
+		filename: filename,
+		arch:     matches[1],
+		iso:      matches[2] == "iso",
+	}, nil
+}
 
 type InvalidBaseImageError struct {
 	cause error
@@ -38,13 +85,13 @@ func (ie InvalidBaseImageError) Unwrap() error {
 // imageFileSystem is an http.FileSystem that creates a virtual filesystem of
 // host images.
 type imageFileSystem struct {
-	isoFile       *baseIso
-	initramfsFile *baseInitramfs
-	baseURL       *url.URL
-	keys          map[string]string
-	images        map[string]*imageFile
-	mu            *sync.Mutex
-	log           logr.Logger
+	isoFiles       map[string]*baseIso
+	initramfsFiles map[string]*baseInitramfs
+	baseURL        *url.URL
+	keys           map[string]string
+	images         map[string]*imageFile
+	mu             *sync.Mutex
+	log            logr.Logger
 }
 
 var _ ImageHandler = &imageFileSystem{}
@@ -52,31 +99,68 @@ var _ http.FileSystem = &imageFileSystem{}
 
 type ImageHandler interface {
 	FileSystem() http.FileSystem
-	ServeImage(key string, ignitionContent []byte, initramfs, static bool) (string, error)
+	ServeImage(key string, arch string, ignitionContent []byte, initramfs, static bool) (string, error)
 	RemoveImage(key string)
 }
 
-func NewImageHandler(logger logr.Logger, isoFile, initramfsFile string, baseURL *url.URL) ImageHandler {
-	return &imageFileSystem{
-		log:           logger,
-		isoFile:       newBaseIso(isoFile),
-		initramfsFile: newBaseInitramfs(initramfsFile),
-		baseURL:       baseURL,
-		keys:          map[string]string{},
-		images:        map[string]*imageFile{},
-		mu:            &sync.Mutex{},
+func NewImageHandler(logger logr.Logger, baseURL *url.URL, envInputs *env.EnvInputs) (ImageHandler, error) {
+	imageFiles, err := os.ReadDir(envInputs.ImageSharedDir)
+
+	if err != nil {
+		return &imageFileSystem{}, err
 	}
+
+	isoFiles := map[string]*baseIso{}
+	initramfsFiles := map[string]*baseInitramfs{}
+
+	logger.Info("reading image files", "dir", envInputs.ImageSharedDir, "len", len(imageFiles))
+	for _, imageFile := range imageFiles {
+		filename := imageFile.Name()
+
+		logger.Info("load image", "imageFile", imageFile.Name())
+
+		ironicImage, err := parseDeployImage(envInputs, filename)
+		if err != nil {
+			logger.Info("failed to parse ironic image, continuing")
+			continue
+		}
+
+		logger.Info("image loaded", "filename", ironicImage.filename, "arch", ironicImage.arch, "iso", ironicImage.iso)
+
+		if ironicImage.iso {
+			isoFiles[ironicImage.arch] = newBaseIso(path.Join(envInputs.ImageSharedDir, filename))
+		} else {
+			initramfsFiles[ironicImage.arch] = newBaseInitramfs(path.Join(envInputs.ImageSharedDir, filename))
+		}
+	}
+
+	return &imageFileSystem{
+		log:            logger,
+		isoFiles:       isoFiles,
+		initramfsFiles: initramfsFiles,
+		baseURL:        baseURL,
+		keys:           map[string]string{},
+		images:         map[string]*imageFile{},
+		mu:             &sync.Mutex{},
+	}, nil
 }
 
 func (f *imageFileSystem) FileSystem() http.FileSystem {
 	return f
 }
 
-func (f *imageFileSystem) getBaseImage(initramfs bool) baseFile {
+func (f *imageFileSystem) getBaseImage(arch string, initramfs bool) baseFile {
+	if arch == "" {
+		arch = hostArchitectureKey
+	}
+
+	f.log.Info("getBaseImage", "arch", arch, "initramfs", initramfs)
 	if initramfs {
-		return f.initramfsFile
+		file := f.initramfsFiles[arch]
+		return file
 	} else {
-		return f.isoFile
+		file := f.isoFiles[arch]
+		return file
 	}
 }
 
@@ -91,8 +175,11 @@ func (f *imageFileSystem) getNameForKey(key string) (name string, err error) {
 	return
 }
 
-func (f *imageFileSystem) ServeImage(key string, ignitionContent []byte, initramfs, static bool) (string, error) {
-	size, err := f.getBaseImage(initramfs).Size()
+func (f *imageFileSystem) ServeImage(key string, arch string, ignitionContent []byte, initramfs, static bool) (string, error) {
+	f.log.Info("ServeImage")
+	baseImage := f.getBaseImage(arch, initramfs)
+
+	size, err := baseImage.Size()
 	if err != nil {
 		return "", InvalidBaseImageError{cause: err}
 	}
@@ -116,6 +203,7 @@ func (f *imageFileSystem) ServeImage(key string, ignitionContent []byte, initram
 		f.keys[name] = key
 		f.images[key] = &imageFile{
 			name:            name,
+			arch:            arch,
 			size:            size,
 			ignitionContent: ignitionContent,
 			initramfs:       initramfs,

pkg/imagehandler/imagehandler_test.go
@@ -23,6 +23,8 @@ import (
 	"testing"
 
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	"github.com/openshift/image-customization-controller/pkg/env"
 )
 
 type closer struct {
@@ -47,8 +49,10 @@ func TestImageHandler(t *testing.T) {
 
 	rr := httptest.NewRecorder()
 	imageServer := &imageFileSystem{
-		log:     zap.New(zap.UseDevMode(true)),
-		isoFile: &baseIso{baseFileData{filename: "dummyfile.iso", size: 12345}},
+		log: zap.New(zap.UseDevMode(true)),
+		isoFiles: map[string]*baseIso{
+			"host": {baseFileData{filename: "dummyfile.iso", size: 12345}},
+		},
 		baseURL: baseURL,
 		keys: map[string]string{
 			"host-xyz-45-uuid": "host-xyz-45.iso",
@@ -86,20 +90,29 @@ func TestNewImageHandler(t *testing.T) {
 	if err != nil {
 		t.Fatalf("unexpected error %v", err)
 	}
-	handler := NewImageHandler(zap.New(zap.UseDevMode(true)),
-		"dummyfile.iso",
-		"dummyfile.initramfs",
-		baseUrl)
 
-	ifs := handler.(*imageFileSystem)
-	ifs.isoFile.size = 12345
-	ifs.initramfsFile.size = 12345
+	ifs := imageFileSystem{
+		baseURL:        baseUrl,
+		keys:           map[string]string{},
+		mu:             &sync.Mutex{},
+		images:         map[string]*imageFile{},
+		isoFiles:       map[string]*baseIso{},
+		initramfsFiles: map[string]*baseInitramfs{},
+	}
 
-	url1, err := handler.ServeImage("test-key-1", []byte{}, false, false)
+	iso := newBaseIso("dummyfile.iso")
+	iso.size = 123456
+	ifs.isoFiles["host"] = iso
+
+	initramfs := newBaseInitramfs("dummyfile.initramfs")
+	initramfs.size = 12345
+	ifs.initramfsFiles["host"] = initramfs
+
+	url1, err := ifs.ServeImage("test-key-1", "", []byte{}, false, false)
 	if err != nil {
 		t.Fatalf("unexpected error %v", err)
 	}
-	url2, err := handler.ServeImage("test-key-2", []byte{}, true, false)
+	url2, err := ifs.ServeImage("test-key-2", "", []byte{}, true, false)
 	if err != nil {
 		t.Fatalf("unexpected error %v", err)
 	}
@@ -109,7 +122,7 @@ func TestNewImageHandler(t *testing.T) {
 		t.Errorf("can't look up image file \"%s\"", name2)
 	}
 
-	url1again, err := handler.ServeImage("test-key-1", []byte{}, false, false)
+	url1again, err := ifs.ServeImage("test-key-1", "", []byte{}, false, false)
 	if err != nil {
 		t.Fatalf("unexpected error %v", err)
 	}
@@ -118,8 +131,8 @@ func TestNewImageHandler(t *testing.T) {
 		t.Errorf("inconsistent URLs for same key: %s %s", url1, url1again)
 	}
 
-	handler.RemoveImage("test-key-1")
-	url1yetagain, err := handler.ServeImage("test-key-1", []byte{}, false, false)
+	ifs.RemoveImage("test-key-1")
+	url1yetagain, err := ifs.ServeImage("test-key-1", "", []byte{}, false, false)
 	if err != nil {
 		t.Fatalf("unexpected error %v", err)
 	}
@@ -133,24 +146,33 @@ func TestNewImageHandlerStatic(t *testing.T) {
 	if err != nil {
 		t.Fatalf("unexpected error %v", err)
 	}
-	handler := NewImageHandler(zap.New(zap.UseDevMode(true)),
-		"dummyfile.iso",
-		"dummyfile.initramfs",
-		baseUrl)
 
-	ifs := handler.(*imageFileSystem)
-	ifs.isoFile.size = 12345
-	ifs.initramfsFile.size = 12345
+	ifs := imageFileSystem{
+		baseURL:        baseUrl,
+		keys:           map[string]string{},
+		mu:             &sync.Mutex{},
+		images:         map[string]*imageFile{},
+		isoFiles:       map[string]*baseIso{},
+		initramfsFiles: map[string]*baseInitramfs{},
+	}
 
-	url1, err := handler.ServeImage("test-name-1.iso", []byte{}, false, true)
+	iso := newBaseIso("dummyfile.iso")
+	iso.size = 123456
+	ifs.isoFiles["host"] = iso
+
+	initramfs := newBaseInitramfs("dummyfile.initramfs")
+	initramfs.size = 12345
+	ifs.initramfsFiles["host"] = initramfs
+
+	url1, err := ifs.ServeImage("test-name-1.iso", "", []byte{}, false, true)
 	if err != nil {
 		t.Fatalf("unexpected error %v", err)
 	}
-	url2, err := handler.ServeImage("test-name-2.initramfs", []byte{}, true, true)
+	url2, err := ifs.ServeImage("test-name-2.initramfs", "", []byte{}, true, true)
 	if err != nil {
 		t.Fatalf("unexpected error %v", err)
 	}
-	url1again, err := handler.ServeImage("test-name-1.iso", []byte{}, false, true)
+	url1again, err := ifs.ServeImage("test-name-1.iso", "", []byte{}, false, true)
 	if err != nil {
 		t.Fatalf("unexpected error %v", err)
 	}
@@ -165,5 +187,73 @@ func TestNewImageHandlerStatic(t *testing.T) {
 	}
 	if url1again != url1 {
 		t.Errorf("inconsistent URLs for same key: %s %s", url1, url1again)
+	}
+}
+
+func TestImagePattern(t *testing.T) {
+	envInputs := &env.EnvInputs{
+		DeployISO:    "/shared/ironic-python-agent.iso",
+		DeployInitrd: "/shared/ironic-python-agent.initramfs",
+	}
+
+	tcs := []struct {
+		name     string
+		filename string
+		arch     string
+		iso      bool
+		error    bool
+	}{
+		{
+
+			name:     "host iso",
+			filename: envInputs.DeployISO,
+			arch:     "host",
+			iso:      true,
+		},
+		{
+
+			name:     "host initramfs",
+			filename: envInputs.DeployInitrd,
+			arch:     "host",
+		},
+		{
+
+			name:     "host initramfs absolute path",
+			filename: "/shared/ironic-python-agent.initramfs",
+			arch:     "host",
+		},
+		{
+
+			name:     "aarch64 iso",
+			filename: "ironic-python-agent_aarch64.iso",
+			arch:     "aarch64",
+			iso:      true,
+		},
+		{
+
+			name:     "aarch64 initramfs",
+			filename: "ironic-python-agent_aarch64.initramfs",
+			arch:     "aarch64",
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Logf("testing %s", tc.name)
+		ii, err := parseDeployImage(envInputs, tc.filename)
+
+		if err != nil && !tc.error {
+			t.Errorf("got error: %v", err)
+			return
+		}
+
+		if ii.arch != tc.arch {
+			t.Errorf("arch: expected %s but got %s", tc.arch, ii.arch)
+			return
+		}
+
+		if ii.iso != tc.iso {
+			t.Errorf("iso: expected %t but got %t", tc.iso, ii.iso)
+			return
+		}
 	}
 }

cmd/controller/main.go
@@ -169,7 +169,11 @@ func main() {
 		envInputs.IronicAgentPullSecret = string(pullSecretRaw)
 	}
 
-	imageServer := imagehandler.NewImageHandler(ctrl.Log.WithName("ImageHandler"), envInputs.DeployISO, envInputs.DeployInitrd, publishURL)
+	imageServer, err := imagehandler.NewImageHandler(ctrl.Log.WithName("ImageHandler"), publishURL, envInputs)
+	if err != nil {
+		setupLog.Error(err, "failed to initialized image handler")
+		os.Exit(1)
+	}
 	http.Handle("/", http.FileServer(imageServer.FileSystem()))
 
 	go func() {

cmd/static-server/main.go
@@ -106,7 +106,7 @@ func loadStaticNMState(fsys fs.FS, env *env.EnvInputs, nmstateDir string, imageS
 			imageName := strings.TrimSuffix(f.Name(), ".yaml") + suffix
 
 			isInitramfs := !strings.HasSuffix(imageName, ".iso")
-			url, err := imageServer.ServeImage(imageName, ign, isInitramfs, true)
+			url, err := imageServer.ServeImage(imageName, "", ign, isInitramfs, true)
 			if err != nil {
 				return err
 			}
@@ -134,7 +134,7 @@ func main() {
 
 	version.Print(log)
 
-	env, err := env.New()
+	envInputs, err := env.New()
 	if err != nil {
 		log.Error(err, "environment not provided")
 		os.Exit(1)
@@ -151,10 +151,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	imageServer := imagehandler.NewImageHandler(ctrl.Log.WithName("ImageHandler"), env.DeployISO, env.DeployInitrd, publishURL)
+	imageServer, err := imagehandler.NewImageHandler(ctrl.Log.WithName("ImageHandler"), publishURL, envInputs)
+	if err != nil {
+		log.Error(err, "failed to initialized image handler")
+		os.Exit(1)
+	}
 	http.Handle("/", http.FileServer(imageServer.FileSystem()))
 
-	if err := loadStaticNMState(os.DirFS("/"), env, nmstateDir, imageServer); err != nil {
+	if err := loadStaticNMState(os.DirFS("/"), envInputs, nmstateDir, imageServer); err != nil {
 		log.Error(err, "problem loading static ignitions")
 		os.Exit(1)
 	}

cmd/static-server/main_test.go
@@ -42,7 +42,7 @@ func (f *fakeImageFileSystem) Seek(offset int64, whence int) (int64, error) { re
 func (f *fakeImageFileSystem) Readdir(n int) ([]fs.FileInfo, error)         { return nil, nil }
 func (f *fakeImageFileSystem) Open(name string) (http.File, error)          { return nil, nil }
 func (f *fakeImageFileSystem) FileSystem() http.FileSystem                  { return f }
-func (f *fakeImageFileSystem) ServeImage(name string, ignitionContent []byte, initrd, static bool) (string, error) {
+func (f *fakeImageFileSystem) ServeImage(name string, arch string, ignitionContent []byte, initrd, static bool) (string, error) {
 	f.imagesServed = append(f.imagesServed, name)
 	return "", nil
 }

pkg/imagehandler/imagefile.go
@@ -26,6 +26,7 @@ type imageFile struct {
 	io.ReadSeekCloser
 	name            string
 	size            int64
+	arch            string
 	ignitionContent []byte
 	imageReader     isoeditor.ImageReader
 	initramfs       bool

pkg/imagehandler/imagefilesystem.go
@@ -56,7 +56,10 @@ func (f *imageFileSystem) Open(name string) (http.File, error) {
 	if im == nil {
 		return nil, fs.ErrNotExist
 	}
-	if err := im.Init(f.getBaseImage(im.initramfs)); err != nil {
+
+	baseImage := f.getBaseImage(im.arch, im.initramfs)
+
+	if err := im.Init(baseImage); err != nil {
 		f.log.Error(err, "failed to create image stream")
 		return nil, err
 	}

commit 3d1b0b9b29dc0faf08af1991f8c2caba0aa51ddf

Always find DEPLOY_ISO and DEPLOY_INITRD
A regression from 583e13f963e7b47fdf8b92c908e65142d14a493b meant that
the DEPLOY_ISO and DEPLOY_INITRD files would not be found unless they
happened to be in the directory specified as IMAGE_SHARED_DIR (by
default /shared/html/images).

This change means that these files will always be found if specified,
and that the directory/directories that contain them will be searched
for arch-specific images unless the search directory is explicitly
overridden by IMAGE_SHARED_DIR.

Assisted-By: Cursor

pkg/env/env.go
@@ -10,7 +10,7 @@ import (
 type EnvInputs struct {
 	DeployISO                 string `envconfig:"DEPLOY_ISO" required:"true"`
 	DeployInitrd              string `envconfig:"DEPLOY_INITRD" required:"true"`
-	ImageSharedDir            string `envconfig:"IMAGE_SHARED_DIR" default:"/shared/html/images"`
+	ImageSharedDir            string `envconfig:"IMAGE_SHARED_DIR"`
 	IronicBaseURL             string `envconfig:"IRONIC_BASE_URL"`
 	IronicInspectorBaseURL    string `envconfig:"IRONIC_INSPECTOR_BASE_URL"`
 	IronicAgentImage          string `envconfig:"IRONIC_AGENT_IMAGE" required:"true"`

pkg/imagehandler/imagehandler.go
@@ -122,34 +122,66 @@ type ImageHandler interface {
 	RemoveImage(key string)
 }
 
-func NewImageHandler(logger logr.Logger, baseURL *url.URL, envInputs *env.EnvInputs) (ImageHandler, error) {
-	imageFiles, err := os.ReadDir(envInputs.ImageSharedDir)
+func findOSImageCandidates(logger logr.Logger, envInputs *env.EnvInputs, filePaths []string) []string {
+	var searchDirs []string
 
-	if err != nil {
-		return &imageFileSystem{}, err
+	if envInputs.ImageSharedDir != "" {
+		searchDirs = append(searchDirs, envInputs.ImageSharedDir)
+
+		if filepath.Dir(envInputs.DeployISO) != envInputs.ImageSharedDir {
+			filePaths = append(filePaths, envInputs.DeployISO)
+		}
+		if filepath.Dir(envInputs.DeployInitrd) != envInputs.ImageSharedDir {
+			filePaths = append(filePaths, envInputs.DeployInitrd)
+		}
+	} else {
+		dirSet := make(map[string]bool)
+		dirSet[filepath.Dir(envInputs.DeployISO)] = true
+		dirSet[filepath.Dir(envInputs.DeployInitrd)] = true
+
+		for dir := range dirSet {
+			searchDirs = append(searchDirs, dir)
+		}
 	}
+
+	for _, searchDir := range searchDirs {
+		imageFiles, err := os.ReadDir(searchDir)
+		if err != nil {
+			logger.Info("failed to read directory, continuing", "dir", searchDir, "error", err)
+			continue
+		}
+		logger.Info("reading image files", "dir", searchDir, "len", len(imageFiles))
+		for _, imageFile := range imageFiles {
+			fullPath := path.Join(searchDir, imageFile.Name())
+			filePaths = append(filePaths, fullPath)
+		}
+	}
+
+	return filePaths
+}
+
+func NewImageHandler(logger logr.Logger, baseURL *url.URL, envInputs *env.EnvInputs) (ImageHandler, error) {
+	filePaths := findOSImageCandidates(logger, envInputs, nil)
 
 	isoFiles := map[string]*baseIso{}
 	initramfsFiles := map[string]*baseInitramfs{}
 
-	logger.Info("reading image files", "dir", envInputs.ImageSharedDir, "len", len(imageFiles))
-	for _, imageFile := range imageFiles {
-		filename := imageFile.Name()
+	logger.Info("processing image files", "total", len(filePaths))
+	for _, filePath := range filePaths {
+		logger.Info("load image", "file", filePath)
 
-		logger.Info("load image", "imageFile", imageFile.Name())
-
-		ironicImage, err := parseDeployImage(envInputs, filename)
+		ironicImage, err := parseDeployImage(envInputs, filePath)
 		if err != nil {
-			logger.Info("failed to parse ironic image, continuing")
+			logger.Info("failed to parse ironic image, continuing", "file", filePath)
 			continue
 		}
 
 		logger.Info("image loaded", "filename", ironicImage.filename, "arch", ironicImage.arch, "iso", ironicImage.iso)
 
 		if ironicImage.iso {
-			isoFiles[ironicImage.arch] = newBaseIso(path.Join(envInputs.ImageSharedDir, filename))
+			isoFiles[ironicImage.arch] = newBaseIso(filePath)
 		} else {
-			initramfsFiles[ironicImage.arch] = newBaseInitramfs(path.Join(envInputs.ImageSharedDir, filename))
+			initramfsFiles[ironicImage.arch] = newBaseInitramfs(filePath)
 		}
 	}
 

pkg/imagehandler/imagehandler_test.go
@@ -298,3 +298,144 @@ func TestImagePattern(t *testing.T) {
 		}
 	}
 }
+
+func TestImagePatternBaseImagesOutsideSharedDir(t *testing.T) {
+	// Test that base deploy images can be located anywhere, not just in ImageSharedDir
+	envInputs := &env.EnvInputs{
+		DeployISO:      "/config/base/ipa.iso",       // Outside shared dir
+		DeployInitrd:   "/config/base/ipa.initramfs", // Outside shared dir
+		ImageSharedDir: "/shared/images",             // Different directory
+	}
+
+	tcs := []struct {
+		name     string
+		filename string
+		arch     string
+		iso      bool
+		error    bool
+	}{
+		{
+			name:     "base iso outside shared dir",
+			filename: "/config/base/ipa.iso",
+			arch:     "host",
+			iso:      true,
+		},
+		{
+			name:     "base initramfs outside shared dir",
+			filename: "/config/base/ipa.initramfs",
+			arch:     "host",
+		},
+		{
+			name:     "arch-specific iso in shared dir",
+			filename: "/shared/images/ipa_aarch64.iso",
+			arch:     "aarch64",
+			iso:      true,
+		},
+		{
+			name:     "arch-specific initramfs in shared dir",
+			filename: "/shared/images/ipa.x86_64.initramfs",
+			arch:     "x86_64",
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Logf("testing %s", tc.name)
+		ii, err := parseDeployImage(envInputs, tc.filename)
+
+		if err != nil && !tc.error {
+			t.Errorf("got error: %v", err)
+			return
+		}
+
+		if err == nil && tc.error {
+			t.Errorf("expected error but got none")
+			return
+		}
+
+		if tc.error {
+			continue
+		}
+
+		if ii.arch != tc.arch {
+			t.Errorf("arch: expected %s but got %s", tc.arch, ii.arch)
+			return
+		}
+
+		if ii.iso != tc.iso {
+			t.Errorf("iso: expected %t but got %t", tc.iso, ii.iso)
+			return
+		}
+	}
+}
+
+func TestImagePatternAutoDiscovery(t *testing.T) {
+	envInputs := &env.EnvInputs{
+		DeployISO:    "/config/base/ipa.iso",
+		DeployInitrd: "/opt/images/ipa.initramfs",
+	}
+
+	tcs := []struct {
+		name     string
+		filename string
+		arch     string
+		iso      bool
+		error    bool
+	}{
+		{
+			name:     "base iso auto-discovery",
+			filename: "/config/base/ipa.iso",
+			arch:     "host",
+			iso:      true,
+		},
+		{
+			name:     "base initramfs auto-discovery",
+			filename: "/opt/images/ipa.initramfs",
+			arch:     "host",
+		},
+		{
+			name:     "arch-specific iso in base iso directory",
+			filename: "/config/base/ipa_aarch64.iso",
+			arch:     "aarch64",
+			iso:      true,
+		},
+		{
+			name:     "arch-specific initramfs in base initramfs directory",
+			filename: "/opt/images/ipa.x86_64.initramfs",
+			arch:     "x86_64",
+		},
+		{
+			name:     "invalid filename in auto-discovered directory",
+			filename: "/config/base/different-file_x86_64.iso",
+			error:    true,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Logf("testing %s", tc.name)
+		ii, err := parseDeployImage(envInputs, tc.filename)
+
+		if err != nil && !tc.error {
+			t.Errorf("got error: %v", err)
+			return
+		}
+
+		if err == nil && tc.error {
+			t.Errorf("expected error but got none")
+			return
+		}
+
+		if tc.error {
+			continue
+		}
+
+		if ii.arch != tc.arch {
+			t.Errorf("arch: expected %s but got %s", tc.arch, ii.arch)
+			return
+		}
+
+		if ii.iso != tc.iso {
+			t.Errorf("iso: expected %t but got %t", tc.iso, ii.iso)
+			return
+		}
+	}
+}